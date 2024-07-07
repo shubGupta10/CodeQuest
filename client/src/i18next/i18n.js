915be38ba1298dfb863468300e2ba3797bf986d9
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-07-07 17:48:56 +0530

    Important comments

--- client/src/i18next/i18n.js
+++ client/src/i18next/i18n.js
@@ -2,13 +2,15 @@ import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import {initReactI18next} from 'react-i18next'
 
-
+//i18n package helps us to create multi language system.
 i18n.use(LanguageDetector).use(initReactI18next).init({
     debug: true,
     lng: "en",
     resources: {
         en: {
-
+            translation: {
+                
+            }
         },
         fr: {
 
