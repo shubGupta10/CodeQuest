28fd85edd3d5901c3627d170e7b34aded6983c1f
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-07-13 14:06:01 +0530

    Fixed issue in Users data fetching

--- client/src/pages/Userprofile/Userprofile.jsx
+++ client/src/pages/Userprofile/Userprofile.jsx
@@ -25,7 +25,7 @@ const Userprofile = ({ slidein }) => {
         <section>
           <div className="user-details-container">
             <div className="user-details">
-              <Avatar backgroundColor="purple" color="white" fontSize="50px" px="40px" py="30px">{currentprofile.name.charAt(0).toUpperCase()}</Avatar>
+              <Avatar backgroundColor="purple" color="white" fontSize="50px" px="40px" py="30px">{currentprofile?.name.charAt(0).toUpperCase()}</Avatar>
               <div className="user-name">
                 <h1>{currentprofile?.name}</h1>
                 <p>
