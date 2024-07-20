d5eb1a7413e2db5e8afce714b0cf6d1e43b7936a
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-07-20 11:35:06 +0530

    Basic Setup to get Browser, OS and IP Adress

---
+++ server/utils/utils.js
@@ -0,0 +1,20 @@
+import UAParser from 'ua-parser-js'
+import requestIp from 'request-ip'
+
+const userInfo = (UserAgent) => {
+    const parser = new UAParser();
+    parser.setUA(UserAgent);
+
+    const browser = parser.getBrowser().name;
+    const os = parser.getOS().name;
+    const device = parser.getDevice().type;
+    return {browser, os, device}
+}
+
+
+const getIPAdress = (req) => {
+    return requestIp.getClientIp(req);
+}
+
+
+export {userInfo, getIPAdress}
