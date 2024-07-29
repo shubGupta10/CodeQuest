58da98b7f48db3c979502e7f7afe1628b009f2d2
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-07-29 12:29:45 +0530

    Fixing bugs

--- client/src/Comnponent/LanguageSwitcher/SendOTPPage.jsx
+++ client/src/Comnponent/LanguageSwitcher/SendOTPPage.jsx
@@ -4,6 +4,7 @@ import './SendOtpPage.css';
 import { useTranslation } from 'react-i18next';
 
 const SendOtpPage = () => {
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || 'https://codequest-emf6.onrender.com';
     const [phoneNumber, setPhoneNumber] = useState('');
     const [error, setError] = useState('');
     const location = useLocation();
@@ -42,7 +43,7 @@ const SendOtpPage = () => {
         setIsLoading(true);
 
         try {
-            const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/user/sending-otp`, {
+            const response = await fetch('backendUrl/user/sending-otp', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
