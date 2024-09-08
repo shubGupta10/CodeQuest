5496c04ecf4a5eb77ae1d6e80648ef210f74cb23
Shubham Kumar Gupta <132940559+shubGupta10@users.noreply.github.com>
2024-09-08 11:51:19 +0530

    Update question.js

--- server/routes/question.js
+++ server/routes/question.js
@@ -5,7 +5,10 @@ import auth from "../middleware/auth.js"
 
 const router=express.Router();
 
+//ask questions route
 router.post('/Ask',auth,Askquestion);
+
+
 router.get('/get',getallquestion);
 router.delete("/delete/:id",auth,deletequestion);
 router.patch("/vote/:id",auth,votequestion)
